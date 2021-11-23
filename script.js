42f580fe88564c85ae84b822d93da84ce97e3d01
Venu4054 <89689050+Venu4054@users.noreply.github.com>
2021-11-23 18:03:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(()=>{
+    document.getElementById("delta").innerHTML=10;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=9;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=8;
+    setTimeout(()=>{
+   document.getElementById("delta").innerHTML=7;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=6;
+    setTimeout(()=>{
+   document.getElementById("delta").innerHTML=5;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=4;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=3;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=2;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML=1;
+    setTimeout(()=>{
+    document.getElementById("delta").innerHTML="Happy Independence Day";
+    
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
